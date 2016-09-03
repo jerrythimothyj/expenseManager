05d73227e40007b94e1a73e0e4ba383e17ae0631
niraj.dewangan <niraj.dewangan1608@gmail.com>
2016-09-03 11:45:02 +0530

    #61

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
   <script src="./client/js/directives/calendar.js"></script>
   <script src="./client/js/services/register.js"></script>
   <script src="./client/js/services/expenses.js"></script>
-  <script src="./clinet/js/miscellaneous/misc.js"></script>
+  <script src="./client/js/miscellaneous/misc.js"></script>
   
 </body>
 </html>
