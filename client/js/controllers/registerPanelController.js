b890e9c8ad71f672d9b321c043705683b219f2a6
niraj.dewangan <niraj.dewangan1608@gmail.com>
2016-09-11 11:58:07 +0530

    #37

--- client/js/controllers/registerPanelController.js
+++ client/js/controllers/registerPanelController.js
@@ -2,11 +2,22 @@
   'use strict';
 angular.module('moneyManager')
     .controller('registerPanelController', function($scope) {
-    	
-
+    	$scope.user = {
+    			email : '',
+    			pwd: '',
+    			confPwd:''
+    	}
         $scope.registerUser = (user) => {
         	
         	$scope.registrationSubmit({submissionDetails: user})
         }
+        $scope.resetForm = () => {
+        	$scope.user = {
+    			email : '',
+    			pwd: '',
+    			confPwd:''
+    		}
+        }
+        
     });
 })(window.angular);
