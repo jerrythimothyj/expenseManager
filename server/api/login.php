4ad12c25d434d2ff705ffb4519dd7670a93ccf7f
Jerry Thimothy J <j.jerrythimothy@tcs.com>
2016-09-22 21:54:56 +0530

    Landing Pages Correction

--- server/api/login.php
+++ server/api/login.php
@@ -2,55 +2,69 @@
 	$validations = new validations();
             
     $validObj = new stdClass();
-    
-    // Email Validation
-    $validObj->validEmail = $validations->validateEmail($userObj->user->email);
-    
-    // Password Validation
-    $validObj->validPwd = $validations->validatePwd($userObj->user->pwd);
-    
+
     $validObj->validAll = 1;
-    if(!$validObj->validEmail || 
-      !$validObj->validPwd)
+    if(!isset($userObj->user->email)) {
+        $validObj->validEmail = 0;
         $validObj->validAll = 0;
-    
+    }
+
+    if(!isset($userObj->user->pwd)) {
+        $validObj->validPwd = 0;
+        $validObj->validAll = 0;
+    }
+
     if($validObj->validAll == 1) {
+    
+        // Email Validation
+        $validObj->validEmail = $validations->validateEmail($userObj->user->email);
         
-        $validObj->validAll = 0;
-        $_SESSION['users_email'] = $userObj->user->email;
+        // Password Validation
+        $validObj->validPwd = $validations->validatePwd($userObj->user->pwd);
         
-        $dbConfig = new dbConfig();
-        $dbConfig->dbConnect();
+        $validObj->validAll = 1;
+        if(!$validObj->validEmail || 
+          !$validObj->validPwd)
+            $validObj->validAll = 0;
         
-        $miscMethods = new miscMethods();
-
-        $validObj->existingUser = 0;
-        $userSql = "SELECT sl FROM users ";
-        $userSql .= "WHERE email='".$_SESSION['users_email']."'";
-        $userSql .= " and password='".$userObj->user->pwd."'";
-
-        
-        $dbResult = $dbConfig->dbQuery($userSql);
-        if ($dbResult->num_rows > 0) {
-            while($dbRow = $dbResult->fetch_assoc()) {
-                $_SESSION['users_sl'] = $dbRow['sl'];
-                $validObj->existingUser = 1;
-            }
-        }
-
-        if($validObj->existingUser == 1)
-        	$validObj->validAll = 1;
-
         if($validObj->validAll == 1) {
-	        $userSql = "INSERT INTO users_log (users_sl,";
-	        $userSql .= "ip,"; 
-	        $userSql .= "time)";
-	        $userSql .= "VALUES ('".$_SESSION['users_sl']."',";
-	        $userSql .= "'".$miscMethods->getIP()."',";
-	        $userSql .= "'".$miscMethods->getDTTM()."')";
-	        
-	        $dbConfig->dbQuery($userSql);
-	    }
+            
+            $validObj->validAll = 0;
+            $_SESSION['users_email'] = $userObj->user->email;
+            
+            $dbConfig = new dbConfig();
+            $dbConfig->dbConnect();
+            
+            $miscMethods = new miscMethods();
+
+            $validObj->existingUser = 0;
+            $userSql = "SELECT sl FROM users ";
+            $userSql .= "WHERE email='".$_SESSION['users_email']."'";
+            $userSql .= " and password='".$userObj->user->pwd."'";
+
+            
+            $dbResult = $dbConfig->dbQuery($userSql);
+            if ($dbResult->num_rows > 0) {
+                while($dbRow = $dbResult->fetch_assoc()) {
+                    $_SESSION['users_sl'] = $dbRow['sl'];
+                    $validObj->existingUser = 1;
+                }
+            }
+
+            if($validObj->existingUser == 1)
+            	$validObj->validAll = 1;
+
+            if($validObj->validAll == 1) {
+    	        $userSql = "INSERT INTO users_log (users_sl,";
+    	        $userSql .= "ip,"; 
+    	        $userSql .= "time)";
+    	        $userSql .= "VALUES ('".$_SESSION['users_sl']."',";
+    	        $userSql .= "'".$miscMethods->getIP()."',";
+    	        $userSql .= "'".$miscMethods->getDTTM()."')";
+    	        
+    	        $dbConfig->dbQuery($userSql);
+    	    }
+        }
     }
 
     $returnObj = $validObj;
--- server/api/newUserRegistration.php
+++ server/api/newUserRegistration.php
@@ -3,68 +3,87 @@
             
     $validObj = new stdClass();
     
-    // Email Validation
-    $validObj->validEmail = $validations->validateEmail($newUserObj->user->email);
-    
-    // Password Validation
-    $validObj->validPwd = $validations->validatePwd($newUserObj->user->pwd);
-    
-    // Confirm Password Validation
-    $validObj->validConfPwd = $validations->validateConfPwd($newUserObj->user->pwd, $newUserObj->user->confPwd);
-    
     $validObj->validAll = 1;
-    if(!$validObj->validEmail || 
-      !$validObj->validPwd || 
-      !$validObj->validConfPwd)
+    if(!isset($newUserObj->user->email)) {
+        $validObj->validEmail = 0;
         $validObj->validAll = 0;
-    
-    if($validObj->validAll == 1) {
-        
-        $_SESSION['users_email'] = $newUserObj->user->email;
-        
-        $dbConfig = new dbConfig();
-        $dbConfig->dbConnect();
-        
-        $miscMethods = new miscMethods();
+    }
 
-        $validObj->existingUser = 0;
-        $newUserSql = "SELECT sl FROM users ";
-        $newUserSql .= "WHERE email='".$_SESSION['users_email']."'";
+    if(!isset($newUserObj->user->pwd)) {
+        $validObj->validPwd = 0;
+        $validObj->validAll = 0;
+    }
+    else if(!isset($newUserObj->user->confPwd)) {
+        $validObj->validConfPwd = 0;
+        $validObj->validAll = 0;
+    }
+
+    if($validObj->validAll == 1) {
+
+        // Email Validation
+        $validObj->validEmail = $validations->validateEmail($newUserObj->user->email);
         
-        $dbResult = $dbConfig->dbQuery($newUserSql);
-        if ($dbResult->num_rows > 0) {
-            while($dbRow = $dbResult->fetch_assoc()) {
-                $_SESSION['users_sl'] = $dbRow['sl'];
-                $validObj->existingUser = 1;
+        // Password Validation
+        $validObj->validPwd = $validations->validatePwd($newUserObj->user->pwd);
+        
+        // Confirm Password Validation
+        $validObj->validConfPwd = $validations->validateConfPwd($newUserObj->user->pwd, $newUserObj->user->confPwd);
+        
+        $validObj->validAll = 1;
+        if(!$validObj->validEmail || 
+          !$validObj->validPwd || 
+          !$validObj->validConfPwd)
+            $validObj->validAll = 0;
+        
+        if($validObj->validAll == 1) {
+            
+            $_SESSION['users_email'] = $newUserObj->user->email;
+            
+            $dbConfig = new dbConfig();
+            $dbConfig->dbConnect();
+            
+            $miscMethods = new miscMethods();
+
+            $validObj->existingUser = 0;
+            $newUserSql = "SELECT sl FROM users ";
+            $newUserSql .= "WHERE email='".$_SESSION['users_email']."'";
+            
+            $dbResult = $dbConfig->dbQuery($newUserSql);
+            if ($dbResult->num_rows > 0) {
+                while($dbRow = $dbResult->fetch_assoc()) {
+                    $_SESSION['users_sl'] = $dbRow['sl'];
+                    $validObj->existingUser = 1;
+                }
             }
+
+            if($validObj->existingUser == 1)
+            	$validObj->validAll = 0;
+
+            if($validObj->validAll == 1) {
+    	        $newUserSql = "INSERT INTO users (email,";
+    	        $newUserSql .= "password,";
+    	        $newUserSql .= "ip,"; 
+    	        $newUserSql .= "time)";
+    	        $newUserSql .= "VALUES ('".$newUserObj->user->email."',";
+    	        $newUserSql .= "'".$newUserObj->user->pwd."',";
+    	        $newUserSql .= "'".$miscMethods->getIP()."',";
+    	        $newUserSql .= "'".$miscMethods->getDTTM()."')";
+    	        
+    	        $dbConfig->dbQuery($newUserSql);
+    	        
+    	        $newUserSql = "SELECT sl FROM users ";
+    	        $newUserSql .= "WHERE email='".$_SESSION['users_email']."'";
+    	        
+    	        $dbResult = $dbConfig->dbQuery($newUserSql);
+    	        if ($dbResult->num_rows > 0) {
+    	            while($dbRow = $dbResult->fetch_assoc()) {
+    	                $_SESSION['users_sl'] = $dbRow['sl'];
+    	                $validObj->validUser = 1;
+    	            }
+    	        }
+    	    }
         }
 
-        if($validObj->existingUser == 1)
-        	$validObj->validAll = 0;
-
-        if($validObj->validAll == 1) {
-	        $newUserSql = "INSERT INTO users (email,";
-	        $newUserSql .= "password,";
-	        $newUserSql .= "ip,"; 
-	        $newUserSql .= "time)";
-	        $newUserSql .= "VALUES ('".$newUserObj->user->email."',";
-	        $newUserSql .= "'".$newUserObj->user->pwd."',";
-	        $newUserSql .= "'".$miscMethods->getIP()."',";
-	        $newUserSql .= "'".$miscMethods->getDTTM()."')";
-	        
-	        $dbConfig->dbQuery($newUserSql);
-	        
-	        $newUserSql = "SELECT sl FROM users ";
-	        $newUserSql .= "WHERE email='".$_SESSION['users_email']."'";
-	        
-	        $dbResult = $dbConfig->dbQuery($newUserSql);
-	        if ($dbResult->num_rows > 0) {
-	            while($dbRow = $dbResult->fetch_assoc()) {
-	                $_SESSION['users_sl'] = $dbRow['sl'];
-	                $validObj->validUser = 1;
-	            }
-	        }
-	    }
     }
 
     $returnObj = $validObj;
