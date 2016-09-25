45509968925894d3d0efafb8ccb96c246dd114da
mangeshrao <raomangesh26@gmail.com>
2016-09-25 12:21:17 +0530

    #115 : Comments

--- client/js/controllers/chosenDateController.js
+++ client/js/controllers/chosenDateController.js
@@ -2,8 +2,54 @@
   'use strict';
 angular.module('moneyManager')
     .controller('chosenDateController', function($scope) {
+       
+        $scope.expenseYears = [];
+        let expenseYearLimit = 10;
+        let currentDate = new Date();
+        for (let ictr = currentDate.getFullYear(); ictr > (currentDate.getFullYear() - expenseYearLimit); ictr--)	 {
+        	$scope.expenseYears.push({
+        		id: ictr, 
+        		name: ictr
+        	});
+ 		}
+
+ 		$scope.expenseDate.year = {
+ 			id: $scope.expenseDate.year,
+ 			name: $scope.expenseDate.year
+ 		}
+
+ 		$scope.expenseMonths = [];
+ 		$scope.monthsName = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+ 		let expenseMonthsLimit = $scope.monthsName.length;
+ 		
+ 		for (let ictr = 1; ictr < expenseMonthsLimit; ictr++) {
+ 			$scope.expenseMonths.push({
+ 				id : ictr,
+ 				name : $scope.monthsName[ictr]
+ 			});
+ 		}
+
+ 		$scope.expenseDate.month = {
+ 			id: $scope.expenseDate.month,
+ 			name: $scope.monthsName[$scope.expenseDate.month]
+ 		}
+
+		$scope.expenseDays = [];
+        let expenseDayLimit = 31;
+        for (let ictr = 1; ictr <= expenseDayLimit; ictr++)	 {
+        	$scope.expenseDays.push({
+        		id: ictr, 
+        		name: ictr
+        	});
+ 		}
+
+ 		$scope.expenseDate.day = {
+ 			id: $scope.expenseDate.day,
+ 			name: $scope.expenseDate.day
+ 		}
+
         $scope.getRecords = (expenseDate) => {
-            let dateObj = expenseDate.year + '/' + expenseDate.month  + '/' + expenseDate.day;
+            let dateObj = expenseDate.year.id + '/' + expenseDate.month.id  + '/' + expenseDate.day.id;
             $scope.getExpenseRecords({expenseDate: dateObj});
         };
     });
