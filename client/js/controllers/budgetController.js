c31bb28e19a8eb317d4bac297547bb65e959d7d4
Jerry Thimothy J <jerrythimothy@gmail.com>
2018-09-02 13:22:00 +0530

    #137 Carry forward and button and link focus colors fixed

--- client/js/controllers/budgetController.js
+++ client/js/controllers/budgetController.js
@@ -1,65 +1,79 @@
 (function(angular) {
-  'use strict';
-angular.module('moneyManager')
-    .controller('budgetController', function($scope, $state, expenseFields, budgetData, budgetService) {
+  "use strict";
+  angular
+    .module("moneyManager")
+    .controller("budgetController", function(
+      $scope,
+      $state,
+      expenseFields,
+      budgetData,
+      budgetService
+    ) {
+      var vm = this;
+      vm.noDay = true;
+      vm.dateChosen = false;
 
-        var vm = this;
-        vm.noDay = true;
-        vm.dateChosen = false;
+      vm.expenseFields = _.sortBy(expenseFields.data, "type");
 
-        vm.expenseFields = _.sortBy(expenseFields.data, 'type');
+      let d = new Date();
+      vm.budgetDate = {
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: 1
+      };
 
-        let d = new Date();
-        vm.budgetDate = {
-            year: d.getFullYear(),
-            month: (d.getMonth() + 1),
-            day: 1
-        }
+      vm.budgetRecords = budgetData.data;
+      vm.emptyRecord = {
+        expenseType: 0,
+        comments: "",
+        amount: 0
+      };
+      vm.budgetRecords.push(angular.copy(vm.emptyRecord));
 
-        vm.budgetRecords = budgetData.data;
-        vm.emptyRecord = {
-            expenseType: 0,
-            comments: '',
-            amount: 0
-        }
-        vm.budgetRecords.push(angular.copy(vm.emptyRecord));
+      vm.budgetSavedRes = {};
 
-        vm.budgetSavedRes = {};
+      vm.getBudgetRecords = budgetDate => {
+        budgetService.getBudget(budgetDate).then(response => {
+          vm.getBudgetValidation = {};
 
-        vm.getBudgetRecords = (budgetDate) => {
-            budgetService.getBudget(budgetDate).then((response) => {
-                vm.getBudgetValidation = {};
+          if (response.data.validAll == 0) {
+            vm.getBudgetValidation = response.data;
+          } else {
+            vm.budgetRecords = response.data;
+            vm.budgetRecords.push(angular.copy(vm.emptyRecord));
 
-                if(response.data.validAll == 0) {
-                    vm.getBudgetValidation = response.data;
-                }
-                else {
-                    vm.budgetRecords = response.data;
-                    vm.budgetRecords.push(angular.copy(vm.emptyRecord));
+            vm.dateChosen = true;
+            vm.budgetSavedRes = {};
+          }
+        });
+      };
 
-                    vm.dateChosen = true;
-                    vm.budgetSavedRes = {};
-                }
-            });
-        }
+      vm.saveBudget = budgetRecords => {
+        let budRecords = angular.copy(budgetRecords);
+        budRecords.pop();
 
-        vm.saveBudget = (budgetRecords) =>{
-            let budRecords = angular.copy(budgetRecords);
-            budRecords.pop();
+        let budgetData = {
+          date: vm.budgetDate.year.id + "/" + vm.budgetDate.month.id + "/" + 1,
+          budget: budRecords
+        };
 
-            let budgetData = {
-                date: vm.budgetDate.year.id + '/' + vm.budgetDate.month.id + '/' + 1,
-                budget: budRecords
-            };
+        budgetService.saveBudget(budgetData).then(response => {
+          vm.budgetSavedRes = response.data;
 
-            budgetService.saveBudget(budgetData).then((response) => {
-                vm.budgetSavedRes = response.data;
+          if (response.data.saveInd == 1) {
+            vm.dateChosen = false;
+          }
+        });
+      };
 
-                if(response.data.saveInd == 1) {
-                    vm.dateChosen = false;
-                }
-            });
-
-        }
+      vm.getLastMonthRecords = budgetDate => {
+        let dateObj =
+          budgetDate.year.id +
+          "/" +
+          budgetDate.month.id +
+          "/" +
+          budgetDate.day.id;
+        vm.getBudgetRecords(budgetService.getLastMonthBudgetDate(dateObj));
+      };
     });
 })(window.angular);
--- client/js/services/budgetService.js
+++ client/js/services/budgetService.js
@@ -1,23 +1,28 @@
 (function(angular) {
-  'use strict';
-angular.module('moneyManager')
-    .service('budgetService', function($http) {
-        this.getBudget = (obj) => {
-            return $http({
-              method: 'POST',
-              url: './server/api.php?api=getBudget',
-              data: {
-                date: obj
-              }
-            });
+  "use strict";
+  angular.module("moneyManager").service("budgetService", function($http) {
+    this.getBudget = obj => {
+      return $http({
+        method: "POST",
+        url: "./server/api.php?api=getBudget",
+        data: {
+          date: obj
         }
+      });
+    };
 
-        this.saveBudget = (obj) => {
-            return $http({
-              method: 'POST',
-              url: './server/api.php?api=saveBudget',
-              data: obj
-            });
-        }
-    });
+    this.saveBudget = obj => {
+      return $http({
+        method: "POST",
+        url: "./server/api.php?api=saveBudget",
+        data: obj
+      });
+    };
+
+    this.getLastMonthBudgetDate = dateObj => {
+      return moment(dateObj)
+        .subtract(1, "months")
+        .format("YYYY/MM/DD");
+    };
+  });
 })(window.angular);
